c7b29d44fafe01fed1f8c2041426842abde4dbf3
System administrator <jacobsanz1@gmail.com>
2024-07-06 18:47:32 -0700

    did some format cleaning

--- packages.nix
+++ packages.nix
@@ -3,7 +3,16 @@
 	environment.systemPackages = with pkgs;[
 		# programing
 		gcc
-
+		cargo
+		rustc
+		go
+		nodejs_22
+		ripgrep
+		lazygit
+		gdu
+		bottom
+		python3
+		tree-sitter  		
 		#text editing
 		vim
 		neovim
