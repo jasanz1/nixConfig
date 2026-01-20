fa1c53481aea3e2faf41a788dde9461d1648c35e
Jacob Sanzone <jacobsanz1@gmail.com>
2026-01-19 20:15:28 -0800

    fixed(hopefully) the mango config

--- modules/desktop/mangowc.nix
+++ modules/desktop/mangowc.nix
@@ -1,62 +1,20 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 with lib;
 
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-       flake-parts.url = "github:hercules-ci/flake-parts";
-    mango = {
-      url = "github:DreamMaoMao/mango";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+  options.modules.desktop.mangowc = {
+    enable = mkEnableOption "MangoWC compositor";
   };
-  outputs =
-    inputs@{ self, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      debug = true;
-      systems = [ "x86_64-linux" ];
-      flake = {
-        nixosConfigurations = {
-          hostname = inputs.nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
 
-              # Add mango nixos module
-              inputs.mango.nixosModules.mango
-              {
-                programs.mango.enable = true;
-              }
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  backupFileExtension = "backup";
-                  users."username".imports =
-                    [
-                      (
-                        { ... }:
-                        {
-                          wayland.windowManager.mango = {
-                            enable = true;
-                            settings = ''
-                              # see config.conf
-                            '';
-                            autostart_sh = ''
-                              # see autostart.sh
-                              # Note: here no need to add shebang
-                            '';
-                          };
-                        }
-                      )
-                    ]
-                    ++ [
-                    ];
-                };
-              }
-            ];
-          };
-        };
-      };
-    };
+  config = mkIf config.modules.desktop.mangowc.enable {
+    environment.systemPackages = with pkgs;
+      [
+        inputs.mango.packages.${pkgs.system}.default
+        foot
+        wofi
+        waybar
+        swaybg
+      ];
+  };
 }
