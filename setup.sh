ef03f5e35b2738edbc2984837a57fcc289904b85
Jaocb <jacobsanz1@gmail.com>
2026-01-23 09:22:57 -0800

    added line to remove config before simlinking dotfiles

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ sudo nixos-rebuild switch --flake /etc/nixos/#$1
 
 git clone https://github.com/jasanz1/.Dotfiles.git ~/.Dotfiles
 
+rm -rf ~/.config
+
 ln -s ~/.Dotfiles/.config ~/.config
 
 git clone https://github.com/jasanz1/Astrovimconfig.git ~/.config/nvim
