5e23a773eaa334490467e49e0e0cbf6fbc3becf4
jacob <jacobsanz1@gmail.com>
2026-02-12 14:11:47 -0800

    installing them the home-manager way

--- users/jacob/home.nix
+++ users/jacob/home.nix
@@ -22,10 +22,6 @@
     # # "Hello, world!" when run.
     # pkgs.hello
 
-				pkgs.bat
-				pkgs.ripgrep
-				pkgs.eza
-				pkgs.fd
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -100,7 +96,11 @@
 			defaultEditor = true;
 
 		};
-  };
+		bat.enable = true;
+		eza.enable = true;
+		ripgrep.enable = true;
+		fd.enable = true;
+	};
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
